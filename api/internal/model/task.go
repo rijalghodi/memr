16503c824f67835335930b1de1d28d1f5333cbcd
Rijal <rijalghodi.dev@gmail.com>
2025-11-17 07:41:03 +0700

    fix timezone sync

--- api/internal/model/task.go
+++ api/internal/model/task.go
@@ -6,8 +6,8 @@ type Task struct {
 	ID          string     `json:"id" gorm:"primaryKey"`
 	ProjectID   *string    `json:"project_id"`
 	UserID      string     `json:"user_id"`
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
+	Title       *string    `json:"title"`
+	Description *string    `json:"description"`
 	Status      int        `json:"status"`
 	SortOrder   *string    `json:"sort_order"`
 	DueDate     *time.Time `json:"due_date"`
